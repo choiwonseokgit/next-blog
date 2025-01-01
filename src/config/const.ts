7adb5ead14ce1123181a89348fb110516318c353
choiwonseokgit <cs0328@hanmail.net>
2025-01-01 20:32:33 +0900

    feat: thumbnail 로고 추가

--- src/config/const.ts
+++ src/config/const.ts
@@ -3,4 +3,4 @@ export const baseDomain = "https://cws0325.vercel.app";
 export const blogTitle = "원석's 개발 블로그";
 export const blogDesc = "개발 경험과 공부한 내용을 기록하는 블로그입니다.";
 
-export const blogThumbnailURL = `${baseDomain}/cws_blog_logo.png`;
+export const blogThumbnailURL = `${baseDomain}/cws_blog_logo_small.png`;
