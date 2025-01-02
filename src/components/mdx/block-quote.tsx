dfae2b7de3169cd3d9b30c3280835ba249990ed6
choiwonseokgit <cs0328@hanmail.net>
2025-01-02 20:18:58 +0900

    chore: mdx/block-quote css 수정

--- src/components/mdx/block-quote.tsx
+++ src/components/mdx/block-quote.tsx
@@ -1,6 +1,6 @@
 export const BlockQuote = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="bg-slate-50 dark:bg-slate-900 border-green-400 dark:border-green-800 ps-4 py-[2px] border-s-4 ">
+    <div className="bg-slate-50 dark:bg-slate-900 border-green-400 dark:border-green-800 p-4 py-[2px] border-s-4 ">
       {children}
     </div>
   );
