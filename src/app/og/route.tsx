f781c65133ead451fd684ba8de2b8d6904eb1cb8
choiwonseokgit <cs0328@hanmail.net>
2025-05-17 19:38:37 +0900

    - requesetAnimationFrame 글 추가

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -4,14 +4,16 @@ import { ImageResponse } from "next/og";
 export function GET(request: Request) {
   const url = new URL(request.url);
   const title = url.searchParams.get("title") || "title";
-  const desc = url.searchParams.get("desc") || "desc";
+  const desc = url.searchParams.get("desc") || "";
 
   return new ImageResponse(
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
         <div tw="flex flex-col items-center">
           <h1 tw="text-8xl font-bold">{makeThumbnailName(title)}</h1>
-          <span tw="text-5xl text-gray-500">{makeThumbnailName(desc)}</span>
+          <span tw="text-5xl text-gray-500">
+            {desc.length > 0 && makeThumbnailName(desc)}
+          </span>
         </div>
       </div>
     ),
--- src/const/category-color.ts
+++ src/const/category-color.ts
@@ -1,6 +1,7 @@
-export const CATEGORY_COLORS: Record<string,string> = {
-  react: "text-blue-600",
-  CSS: "text-yellow-400",
-  nextjs_blog: "text-sky-600",
-  financial_calendar: "text-green-600",
+export const CATEGORY_COLORS: Record<string, string> = {
+  react: "text-blue-500",
+  CSS: "text-indigo-400",
+  nextjs_blog: "text-neutral-600",
+  financial_calendar: "text-emerald-600",
+  javascript: "text-yellow-500",
 };
